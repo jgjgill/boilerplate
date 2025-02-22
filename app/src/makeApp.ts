21d565399ed9d21c78fdf626c7c3adb9d92c118d
jgjgill <dbdltm22@naver.com>
2025-02-22 19:49:08 +0900

    anything

--- app/src/makeApp.ts
+++ app/src/makeApp.ts
@@ -1,17 +1,26 @@
 import path from "node:path";
-import fastifyAutoload from "@fastify/autoload";
+import FastifyAutoload from "@fastify/autoload";
 import fastify from "fastify";
 
-export function makeApp() {
+export async function makeApp() {
   const app = fastify({ logger: true });
 
-  app.register(fastifyAutoload, {
+  /**
+   * 플러그인을 등록합니다
+   */
+  await app.register(FastifyAutoload, {
     dir: path.resolve("./dist/plugins"),
   });
 
   app.get("/healthz", async () => {
     return { ok: true };
   });
+
+  /**
+   * 모든 Fastify 플러그인이 준비될때까지 대기합니다
+   */
+  await app.ready();
+  app.log.info({}, "[system] completed - app.ready()");
 
   return app;
 }
--- app/src/web/routes/home.tsx
+++ app/src/web/routes/home.tsx
@@ -1,7 +1,3 @@
-import type { Route } from "./+types/home";
-
-export function loader({ request, context, params }: Route.LoaderArgs) {}
-
 export default function Home() {
-  return <div>Hello World!123</div>;
+  return <div>Hello World</div>;
 }
